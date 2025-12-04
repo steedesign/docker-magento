e5da8f7e0f17385848297cd28022b66d0b748a9a
steedesign <info@steedesign.nl>
2025-12-04 10:50:30 +0100

    Implement authentication check for Magento install
    Added authentication check for Magento repository keys.

--- render/install-magento.sh
+++ render/install-magento.sh
@@ -2,8 +2,18 @@
 
 echo "Starting Magento install..."
 
+# 1) Composer authenticatie voor repo.magento.com
+if [ -z "$MAGENTO_REPO_PUBLIC_KEY" ] || [ -z "$MAGENTO_REPO_PRIVATE_KEY" ]; then
+  echo "ERROR: MAGENTO_REPO_PUBLIC_KEY en/of MAGENTO_REPO_PRIVATE_KEY niet gezet."
+  exit 1
+fi
+
+composer config --global http-basic.repo.magento.com "$MAGENTO_REPO_PUBLIC_KEY" "$MAGENTO_REPO_PRIVATE_KEY"
+
+# 2) Magento project ophalen
 composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition .
 
+# 3) Magento installatie
 bin/magento setup:install \
   --base-url="$MAGENTO_BASE_URL" \
   --db-host="$MAGENTO_DB_HOST" \
